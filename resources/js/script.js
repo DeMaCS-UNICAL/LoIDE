6addebcd599abd22b926981160d2b7c909711717
brady994 <eliana_palermiti@libero.it>
2016-10-12 16:10:59 +0200

    added file upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,29 @@
 require.config({
-          baseUrl: window.location.protocol + "//" + window.location.host + window.location.pathname.split("/").slice(0, -1).join("/"),
-          paths: {
-              ace: "ace/lib/ace"
-          }
-      });
-      require(["ace/ace"], function (ace) {
-        var editor = ace.edit("editor");
-        editor.setTheme("ace/theme/tomorrow");
-        editor.getSession().setMode("ace/mode/Asp");
+    baseUrl: window.location.protocol + "//" + window.location.host + window.location.pathname.split("/").slice(0, -1).join("/"),
+    paths: {
+        ace: "ace/lib/ace"
+    }
+});
+require(["ace/ace"], function (ace) {
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/tomorrow");
+    editor.getSession().setMode("ace/mode/Asp");
+
+});
+$(document).ready(function () {
+    $('.fileinput-upload-button').click(function (e) {
+        var fileToLoad = document.getElementById("vvv").files[0];
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            var text = event.target.result;
+            require(["ace/ace"], function (ace) {
+                var editor = ace.edit("editor");
+                editor.setValue(text);
+
+            });
+
+        };
+        reader.readAsText(fileToLoad, "UTF-8");
+
     });
+});

ccf92f1d36f29cb9b2fb4dacc1dd5edcc41b7f4a
brady994 <eliana_palermiti@libero.it>
2016-10-25 15:19:51 +0200

    Added jQuery UI Layout, Renamed server.js with app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static(path.join(__dirname, 'resources')));
+var request = require('request');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/run', function (req, res) {
+    var data = req.body; //take data and options from http request and save it to a local variable
+
+    request.post({
+            url: 'http://localhost:8080/EmbASPExecutor/home', // send data to EmbASPExecutor in json format
+            method: 'POST',
+            json: data
+        },
+        function (err, httpResponse, body) {
+            if (!err)
+                res.send(body); //send the solution of the data by http rosponse to the gui
+        });
+
+
+});
+
+
+
+app.listen(8084, function () {
+    console.log('App listening on port 8084!');
+});

59a34bc230e9e6f34e257b7947ba721ea6d5a6e2
Stefano Germano <stefanogermano0@gmail.com>
2018-01-02 17:04:20 +0100

    Improved debug string

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ io.sockets.on('connection', function (socket) { // Wait for the incoming connect
         };
         client.onmessage = function (output) { // Wait for the incoming data from the EmbASPServerExecutor
             var model = JSON.parse(output.data);
-            console.log(model + " from EmbASPServerExecutor"); // debug string
+//          console.log("%j from EmbASPServerExecutor", model); // debug string
+            console.log('From EmbASPServerExecutor: "model":"%s", "error":"%s"', model.model, model.error); // debug string
             socket.emit('output', model); // Socket.io calls emit() to send data to the browser.
 
         };

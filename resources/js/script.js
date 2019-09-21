68aa91c43d6597d0f4fe218e4a20af583c52c2bc
Roky97 <roccopalermiti@virgilio.it>
2019-09-21 22:44:16 +0200

    Fixed url decoder

--- resources/js/script.js
+++ resources/js/script.js
@@ -2100,7 +2100,7 @@ function getParameterByName(name, url) {
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    return results[2];
 }
 
 function loadFromURL() {

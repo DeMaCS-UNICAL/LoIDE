a01a2e7f6b40fbda8e4fd1d6f71bc0a9ff0dbfd0
Roky97 <roccopalermiti@virgilio.it>
2020-06-21 19:49:05 +0200

    Add livereload for static resources with Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
 const nodemon = require('gulp-nodemon')
+var browserSync = require('browser-sync').create();
 
 const path =  {
-    dest: 'dist/',
+    dist: 'dist/',
     src: 'resources/'
 }
 
@@ -17,8 +18,12 @@ const environment =  {
     prod: 'production'
 }
 
+// System config loading
+var properties  = require('./config/app-config.json');
+var httpPort    = properties.port.http;
+
 function cleanDir(){
-    return src( path.dest +  '*', {read: false})
+    return src( path.dist +  '*', {read: false})
     .pipe(clean());
 }
 
@@ -29,57 +34,89 @@ function css() {
     }))
     .pipe(shorthand())
     .pipe(csso())
-    .pipe(dest(path.dest + 'css/'))
+    .pipe(dest(path.dist + 'css/'))
 }
 
 function faviconImage() {
     return src(path.src + 'favicon/*.{png,svg}')
     .pipe(imagemin())
-    .pipe(dest(path.dest + 'favicon/'))
+    .pipe(dest(path.dist + 'favicon/'))
 }
 
 function faviconFiles() {
     return src(path.src + 'favicon/*.{ico,xml,webmanifest}')
-    .pipe(dest(path.dest + 'favicon/'))
+    .pipe(dest(path.dist + 'favicon/'))
 }
 
 function img() {
     return src(path.src + 'img/*.*')
     .pipe(imagemin())
-    .pipe(dest(path.dest + 'img/'))
+    .pipe(dest(path.dist + 'img/'))
 }
 
 function js() {
     return src(path.src + 'js/**/*.js')
     .pipe(uglify())
-    .pipe(dest(path.dest + 'js/'))
+    .pipe(dest(path.dist + 'js/'))
 }
 
 function pug() {
     return src(path.src + '**/*.pug')
-    .pipe(dest(path.dest))
+    .pipe(dest(path.dist))
 }
 
-async function serveProd() {
-    nodemon({
+function serveProd(done) {
+    const server = nodemon({
         script: 'app.js'
       , ext: 'js json'
       , ignore: ['node_modules/', 'dist/', 'resources/', 'gulpfile.js']
       , env: { 'NODE_ENV': environment.prod }
       });
+
+      server.on('start', () => {
+        done();
+    });
 }
 
-async function serveDev() {
-    nodemon({
+function serveDev(done) {
+    const STARTUP_TIMEOUT = 5000;
+    const server = nodemon({
         script: 'app.js'
-      , ext: 'js json'
-      , ignore: ['node_modules/', 'dist/', 'resources/', 'gulpfile.js']
-      , env: { 'NODE_ENV': environment.dev }
-      });
+        , stdout: false
+        , ext: 'js json'
+        , ignore: ['node_modules/', 'dist/', 'resources/', 'gulpfile.js']
+        , env: { 'NODE_ENV': environment.dev }
+    });
+    let starting = false;
+
+    const onReady = () => {
+        starting = false;
+        done();
+    };
+    
+    server.on('start', () => {
+        starting = true;
+        setTimeout(onReady, STARTUP_TIMEOUT);
+    });
+    
+    server.on('stdout', (stdout) => {
+        process.stdout.write(stdout); // pass the stdout through
+        if (starting) {
+          onReady();
+        }
+    });   
+}
+
+function startBrowserSync(done){
+    browserSync.init({
+      proxy: "http://localhost:" + httpPort,
+      files: [path.src + "/**/*.*"],
+      port: 7000,
+    }, done);
 }
 
 const build = parallel(css,faviconImage,faviconFiles,img,js,pug);
 
 exports.default = series(cleanDir, build, serveProd)
-exports.dev = series(cleanDir, serveDev)
+exports.dev = series(cleanDir, serveDev, startBrowserSync)
 exports.clean = series(cleanDir)
